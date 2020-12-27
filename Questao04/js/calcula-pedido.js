451e86b024dda5e27e76d34f2639043724c8762e
VitorMaverick <vitor.maverick@acad.ifma.edu.br>
2020-12-27 13:56:21 -0300

    Atividade finalizada

--- Questao04/js/calcula-pedido.js
+++ Questao04/js/calcula-pedido.js
@@ -1,10 +1,11 @@
 let bebidas = document.querySelectorAll(".bebidas");
 let comidas = document.querySelectorAll(".comidas");
-
+let nome = document.querySelector("#js-nome");
 let resultado = document.querySelector('#js-resultado')
 
 function limpaFormulario() {
-    
+
+    nome.value = "";
     for(i=0; bebidas.length > i; i++){
         if(bebidas[i].checked == true){
             bebidas[i].checked = false;
@@ -31,11 +32,15 @@ document
 
     for(i=0; bebidas.length > i; i++){
         if(bebidas[i].checked == true){
-            nomeBebida = bebidas[i].name;
+            nomeBebida = bebidas[i].id;
             valorBebida = bebidas[i].value
         }
         if(comidas[i].checked == true){
-            nomesComidas = nomesComidas + " | " + comidas[i].name;
+            if(nomesComidas == ""){
+                nomesComidas = comidas[i].name;
+            }else{
+                nomesComidas = nomesComidas + " & " + comidas[i].name;
+            }
             valorComida = valorComida + parseFloat(comidas[i].value);
         }
 
@@ -43,8 +48,9 @@ document
 
     valorTotal = valorComida + parseFloat(valorBebida);
 
-    resultado.innerHTML = nomeBebida + " " + valorBebida + " " +  " " + valorTotal + nomesComidas;
+    resultado.innerHTML = `<p class="alert alert-success"> Nome: ${nome.value} | Bebida: ${nomeBebida} | Lanches: ${nomesComidas} | Valor total: ${valorTotal} </p>`
     
+
     limpaFormulario();
 
 });
